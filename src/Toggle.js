9f6afa371cca331d821b0e1251ef62abfe48a592
pinglu85 <luping8576@hotmail.com>
2019-12-07 05:07:19 +0100

    use interpolations to emulate css keyframes

--- src/Toggle.js
+++ src/Toggle.js
@@ -7,13 +7,18 @@ const Toggle = () => {
     // opacity: isToggled ? 1 : 0,
     // fontSize: isToggled ? '2rem' : '5rem',
     color: isToggled ? 'tomato' : 'green',
-    y: isToggled ? 0 : -50
+    y: isToggled ? 0 : 1
   });
   return (
     <div>
       <animated.h1
         style={{
-          transform: y.interpolate(y => `translate3d(0, ${y}px, 0)`),
+          transform: y
+            .interpolate({
+              range: [0, 0.25, 0.5, 0.75, 1],
+              output: [0, -25, -50, -100, -50]
+            })
+            .interpolate(y => `translate3d(0, ${y}px, 0)`),
           color
         }}
       >
