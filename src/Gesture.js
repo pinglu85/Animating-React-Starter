ebf90edf7104239ea18110a05d8c33835b38b27c
pinglu85 <luping8576@hotmail.com>
2019-12-19 20:56:03 +0100

    make the box only slide left and right. fade the box opacity based on the box movement. make the box stick when it moves certain distance

--- src/Gesture.js
+++ src/Gesture.js
@@ -4,11 +4,21 @@ import { animated, useSpring } from 'react-spring';
 import { useDrag } from 'react-use-gesture';
 
 const Gesture = () => {
-  const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }));
+  const [{ x }, set] = useSpring(() => ({ x: 0 }));
   const bind = useDrag(({ down, movement, velocity }) => {
     velocity = clamp(velocity, 1, 8);
+    if (down) {
+      set({ x: movement[0] });
+    } else {
+      if (movement[0] > 400) {
+        set({ x: 500 });
+      } else if (movement[0] < -400) {
+        set({ x: -500 });
+      } else {
+        set({ x: 0 });
+      }
+    }
     set({
-      xy: down ? movement : [0, 0],
       config: { mass: velocity, tension: 500 * velocity, friction: 50 }
     });
   });
@@ -16,7 +26,14 @@ const Gesture = () => {
     <animated.div
       {...bind()}
       style={{
-        transform: xy.interpolate((x, y) => `translate3d(${x}px, ${y}px, 0)`)
+        opacity: x.interpolate({
+          // turns negative value to absolute value,
+          // so that moving left applies the same output.
+          // map: Math.abs,
+          range: [-400, 0, 400],
+          output: [0, 1, 0]
+        }),
+        transform: x.interpolate(x => `translate3d(${x}px, 0, 0)`)
       }}
       className="box"
     >
